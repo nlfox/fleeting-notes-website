7c0437eacc953c09b4a1d75e16713eac9acb8303
Matthew Wong <matthewwong525@gmail.com>
2022-12-08 14:40:01 -0500

    update header & footer + add logo

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -48,16 +48,7 @@ const Header = () => {
           <div className="shrink-0 mr-4">
             {/* Logo */}
             <Link href="/" className="block" aria-label="Fleeting Notes">
-              <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
-                    <stop stopColor="#4FD1C5" offset="0%" />
-                    <stop stopColor="#81E6D9" offset="25.871%" />
-                    <stop stopColor="#338CF5" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
-              </svg>
+              <img className="w-8 h-8" src="/assets/logo-transparent.png"/>
             </Link>
           </div>
 
@@ -70,14 +61,6 @@ const Header = () => {
                   <Link href="/pricing" className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Pricing</Link>
                 </li>
                 <li>
-                  <Link href="/about" className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">About us</Link>
-                </li>
-                <li>
-                  <Link href="/tutorials" className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">
-                    Tutorials
-                  </Link>
-                </li>
-                <li>
                   <Link href="/posts" className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Blog</Link>
                 </li>              
               </ul>
@@ -85,11 +68,8 @@ const Header = () => {
               {/* Desktop sign in links */}
               <ul className="flex grow justify-end flex-wrap items-center">
                 <li>
-                  <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
-                </li>
-                <li>
-                  <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                    <span>Sign up</span>
+                  <Link href="/download" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                    <span>Download Now</span>
                     <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                     </svg>                  
@@ -138,20 +118,11 @@ const Header = () => {
                   <Link href="/pricing" className="flex text-gray-600 hover:text-gray-900 py-2">Pricing</Link>
                 </li>
                 <li>
-                  <Link href="/about" className="flex text-gray-600 hover:text-gray-900 py-2">About us</Link>
-                </li>
-                <li>
-                  <Link href="/tutorials" className="flex text-gray-600 hover:text-gray-900 py-2">Tutorials</Link>
-                </li>  
-                <li>
                   <Link href="/posts" className="flex text-gray-600 hover:text-gray-900 py-2">Blog</Link>
                 </li>                                  
                 <li>
-                  <Link href="/signin" className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center">Sign in</Link>
-                </li>
-                <li>
-                  <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2">
-                    <span>Sign up</span>
+                  <Link href="/download" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2">
+                    <span>Download Now</span>
                     <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fillRule="nonzero" />
                     </svg>

60d96c8e0748ec16bb749960bd8d966fb05968e5
Matthew Wong <matthewwong525@gmail.com>
2022-12-08 14:13:51 -0500

    fix warning

--- components/blog/pagination.tsx
+++ components/blog/pagination.tsx
@@ -24,30 +24,28 @@ function Pagination({ currPage, maxPage }: Props) {
   return (
     <div className="flex justify-center mb-10">
         <ul className="inline-flex -space-x-px">
-          <li>
+          <li key="previous">
             {(currPage === 1) ? (
-              <a href={`javascript:void(0)`}
-                className="bg-white border border-gray-300 text-gray-500 ml-0 rounded-l-lg leading-tight py-2 px-3 cursor-default">Previous</a>
+              <a className="bg-white border border-gray-300 text-gray-500 ml-0 rounded-l-lg leading-tight py-2 px-3 cursor-default">Previous</a>
             ): (
               <a href={`/posts/${currPage - 1}`}
                 className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 ">Previous</a>
             )} 
           </li>
           {paginationArr.map((i) => (i === currPage) ? (
-            <li>
+            <li key={i}>
               <a href="" aria-current="page"
                 className="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700 py-2 px-3">{i}</a>
             </li>
           ) : (
-            <li>
+            <li key={i}>
               <a href={`/posts/${i}`}
                 className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 ">{i}</a>
             </li>
           ))}
-          <li>
+          <li key="next">
             {(currPage === maxPage) ? (
-              <a href={`javascript:void(0)`}
-                className={`bg-white border border-gray-300 text-gray-500 rounded-r-lg leading-tight py-2 px-3 cursor-default`}>Next</a>
+              <a className={`bg-white border border-gray-300 text-gray-500 rounded-r-lg leading-tight py-2 px-3 cursor-default`}>Next</a>
             ): (
               <a href={`/posts/${currPage + 1}`}
                 className={`bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3`}>Next</a>

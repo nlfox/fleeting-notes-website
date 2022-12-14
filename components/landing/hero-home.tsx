4e08fbd86600d3cdec2c6f6ee4e78725096839c0
Matthew Wong <matthewwong525@gmail.com>
2022-12-14 09:30:10 -0500

    update click text

--- components/landing/hero-home.tsx
+++ components/landing/hero-home.tsx
@@ -65,7 +65,7 @@ const HeroHome = () => {
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
-                <span className="ml-3">Click for interactive demo</span>
+                <span className="ml-3">Click to try yourself</span>
               </button>
             </div>
           </div>

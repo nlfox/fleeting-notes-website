37598d9a13dd02495edf02140e419e5accff1a49
Matthew Wong <matthewwong525@gmail.com>
2023-01-06 23:48:05 -0500

    add subscription form

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -1,14 +1,37 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 function Footer() {
+  const [newsletterSubbed, setNewsletterSubbed] = useState(false);
+  const subscribeNewsletter = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const resp = await fetch("https://track.customer.io/api/v1/forms/newsletter_form/submit", {
+      method: "POST",
+      headers: {
+        "Authorization": `Basic ${process.env.NEXT_PUBLIC_CUSTOMER_IO_FORM_AUTH}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "data" : {
+          email,
+          "newsletter_sub": "true"
+        }
+      })
+    });
+    if (resp.ok) {
+      setNewsletterSubbed(true);
+    } else {
+      console.log(await resp.json())
+    }
+  }
   return (
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Top area: Blocks */}
+          <div className="grid sm:grid-cols-12 pt-8 gap-8 md:pt-12 border-t border-gray-200">
           {/* 1st block */}
-          <div className="pt-8 md:pt-12 border-t border-gray-200 w-full">
+          <div className=" sm:col-span-12 md:col-span-6">
             <div className="mb-2">
               {/* Logo */}
               <Link href="/" className="inline-block" aria-label="Cruip">
@@ -23,6 +46,33 @@ function Footer() {
               <Link href="[...slug]" as="faq" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">FAQ</Link>
             </div>
           </div>
+          <div className="sm:col-span-12 md:col-start-9 md:col-end-13 lg:col-start-10">
+            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
+            <p className="text-sm text-gray-600 mb-4">Get the latest news and articles to your inbox every month.</p>
+            <form
+              id="newsletter_form"
+              onSubmit={subscribeNewsletter}
+            >
+              <div className="flex flex-wrap mb-4">
+                <div className="w-full">
+                  <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
+                  <div className="relative flex items-center max-w-xs">
+                    <input id="newsletter" name="email" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
+                    <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
+                      <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
+                      <svg className="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                      </svg>
+                    </button>
+                  </div>
+                  {/* Success message */}
+                  {newsletterSubbed && <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p>}
+                </div>
+              </div>
+            </form>
+          </div>    
+
+          </div>      
 
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8">

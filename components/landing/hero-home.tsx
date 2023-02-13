2d15496cc7a4ad0113610416aeb97f9bc295a8c6
Matthew Wong <matthewwong525@gmail.com>
2023-02-13 16:23:49 -0500

    update header desc

--- components/landing/hero-home.tsx
+++ components/landing/hero-home.tsx
@@ -47,7 +47,7 @@ const HeroHome = () => {
             <h1 className="text-6xl md:text-7xl font-extrabold leading-tight mb-4" data-aos="zoom-y-out">Transform Your Thoughts into <PreviewLink href="/posts/how-i-take-fleeting-notes-with-obsidian">Lasting Knowledge</PreviewLink></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
-              Effortlessly capture your thoughts, ideas, and tasks with Fleeting Notes. And with <PreviewLink href="/posts/sync-fleeting-notes-with-obsidian">seamless integration to Obsidian</PreviewLink>, transform them into lasting knowledge that you can access anytime, anywhere.
+              Effortlessly capture your thoughts, ideas, and tasks with Fleeting Notes. And with <PreviewLink href="/posts/sync-fleeting-notes-with-obsidian">automatic Obsidian sync</PreviewLink>, transform them into lasting knowledge that you can access anytime, anywhere.
               </p>
             </div>
           </div>

4beb7cf8ce74e3bcaeceb26d6ecc7c94e4fd4147
Matthew Wong <matthewwong525@gmail.com>
2023-03-15 13:48:40 -0400

    move pw reset to fleetingnotes.app website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import Head from "next/head";
+import { createClient } from "@supabase/supabase-js";
+import { useEffect } from "react";
 import Cta from "../components/landing/cta";
 import FeaturesBlocks from "../components/landing/features-blocks";
 import FeaturesHome from "../components/landing/features-home";
@@ -7,6 +8,29 @@ import TestimonialsHome from "../components/landing/testimoninals-home";
 import Layout from "../components/misc/layout";
 
 export default function Home() {
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY as string);
+
+  // handle password resets
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      console.log(event);
+      console.log(session?.user.email);
+      if (event === "PASSWORD_RECOVERY") {
+        const newPassword = prompt("What would you like your new password to be?") || undefined;
+        const { data, error } = await supabase.auth
+          .updateUser({ password: newPassword });
+ 
+        if (data) alert("Password updated successfully!");
+        if (error) alert("There was an error updating your password.");
+      }
+    });
+    // if has error:
+    if (window.location.hash.includes("error=")) {
+      alert('Invalid password reset link. Try with a new link~');
+    }
+  }, [supabase.auth]);
+  
+
   return (
     <Layout>
       <HeroHome />

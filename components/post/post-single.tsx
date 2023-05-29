9e1e97e255c50012ea5860e9cbf9f1cce4d925d3
Matthew Wong <matthewwong525@gmail.com>
2023-05-29 15:38:44 -0400

    refactor margins for post-single

--- components/post/post-single.tsx
+++ components/post/post-single.tsx
@@ -25,55 +25,48 @@ function PostSingle({
   backlinks,
 }: Props) {
   return (
-    <section>
-      <div className="max-w-3xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          <div className="max-w-3xl mx-auto lg:max-w-none">
-            <article>
-              {/* Article header */}
-              <header className="max-w-3xl mx-auto mb-20">
-                {/* Title */}
-                <h1 className="h1 text-center mb-4 text-6xl">{title}</h1>
-              </header>
+    <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+      <div className="max-w-3xl mx-auto lg:max-w-none">
+        <article>
+          {/* Article header */}
+          <header className="max-w-3xl mx-auto mb-20">
+            {/* Title */}
+            <h1 className="h1 text-center mb-4 text-6xl">{title}</h1>
+          </header>
 
-              {/* Article content */}
-              <div className="lg:flex lg:justify-between" data-sticky-container>
-                {/* Main content */}
-                <div>
-                  {/* Article meta */}
-                  {(author || date) && (
-                    <>
-                      <PostMeta author={author} date={date} />
-                      <hr className="w-16 h-px pt-px bg-gray-200 border-0 my-6" />
-                    </>
-                  )}
-
-                  {/* Article body */}
-                  <PostBody content={content} />
-                </div>
-              </div>
-
-              {/* Article footer */}
-            </article>
-
-            {/* Backlinks */}
-            {(Object.keys(backlinks).length > 0) && (
-              <div>
-                <hr className="my-8 border border-dashed lg:block" />
-                <h3 className="h3 mb-4">
-                  Backlinks
-                </h3>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                  <Backlinks backlinks={backlinks} />
-                </div>
-              </div>
+          {/* Article content */}
+          <div>
+            {/* Article meta */}
+            {(author || date) && (
+              <>
+                <PostMeta author={author} date={date} />
+                <hr className="w-16 h-px pt-px bg-gray-200 border-0 my-6" />
+              </>
             )}
 
-            {/* End of Backlinks */}
+            {/* Article body */}
+            <PostBody content={content} />
           </div>
-        </div>
+
+          {/* Article footer */}
+        </article>
+
+        {/* Backlinks */}
+        {(Object.keys(backlinks).length > 0) && (
+          <div>
+            <hr className="my-8 border border-dashed lg:block" />
+            <h3 className="h3 mb-4">
+              Backlinks
+            </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Backlinks backlinks={backlinks} />
+            </div>
+          </div>
+        )}
+
+        {/* End of Backlinks */}
       </div>
-    </section>
+    </div>
   );
 }
 
--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,38 +1,36 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import { getPostBySlug, getAllPosts, getLinksMapping } from '../lib/api'
-import Head from 'next/head'
-import { markdownToHtml } from '../lib/markdownToHtml'
-import type PostType from '../interfaces/post'
-import path from 'path'
-import PostSingle from '../components/post/post-single'
-import Layout from '../components/misc/layout'
-import Comments from '../components/blog/comments'
-import { NextSeo } from 'next-seo'
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import { getAllPosts, getLinksMapping, getPostBySlug } from "../lib/api";
+import Head from "next/head";
+import { markdownToHtml } from "../lib/markdownToHtml";
+import type PostType from "../interfaces/post";
+import path from "path";
+import PostSingle from "../components/post/post-single";
+import Layout from "../components/misc/layout";
+import Comments from "../components/blog/comments";
+import { NextSeo } from "next-seo";
 
 type Items = {
-  title: string,
-  excerpt: string,
-}
+  title: string;
+  excerpt: string;
+};
 
 type Props = {
-  post: PostType
-  slug: string
-  backlinks: { [k: string]: Items }
-}
+  post: PostType;
+  slug: string;
+  backlinks: { [k: string]: Items };
+};
 
 export default function Post({ post, backlinks }: Props) {
-  const router = useRouter()
-  const description = post.excerpt.slice(0, 155)
-  const absUrl = path.join('https://fleetingnotes.app', router.asPath)
+  const router = useRouter();
+  const description = post.excerpt.slice(0, 155);
+  const absUrl = path.join("https://fleetingnotes.app", router.asPath);
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
   return (
     <>
-      {router.isFallback ? (
-        <h1>Loading…</h1>
-      ) : (
+      {router.isFallback ? <h1>Loading…</h1> : (
         <Layout>
           <NextSeo
             title={post.title}
@@ -41,56 +39,64 @@ export default function Post({ post, backlinks }: Props) {
             openGraph={{
               title: post.title,
               description,
-              type: 'article',
+              type: "article",
               url: absUrl,
               images: [{
-                url: (post.ogImage?.url) ? post.ogImage.url : "https://fleetingnotes.app/favicon/512.png",
-                width: (post.ogImage?.url) ? null: 512,
-                height: (post.ogImage?.url) ? null: 512,
-                type: null
-              }]
+                url: (post.ogImage?.url)
+                  ? post.ogImage.url
+                  : "https://fleetingnotes.app/favicon/512.png",
+                width: (post.ogImage?.url) ? null : 512,
+                height: (post.ogImage?.url) ? null : 512,
+                type: null,
+              }],
             }}
           />
-          <PostSingle
-            title={post.title}
-            content={post.content}
-            date={post.date}
-            author={post.author}
-            backlinks={backlinks}
-          />
-          <Comments />
+          <div className="mx-auto max-w-3xl">
+            <PostSingle
+              title={post.title}
+              content={post.content}
+              date={post.date}
+              author={post.author}
+              backlinks={backlinks}
+            />
+            <Comments />
+          </div>
         </Layout>
       )}
     </>
-  )
+  );
 }
 
 type Params = {
   params: {
-    slug: string[]
-    backlinks: string[]
-  }
-}
+    slug: string[];
+    backlinks: string[];
+  };
+};
 
 export async function getStaticProps({ params }: Params) {
-  const slug = path.join(...params.slug)
+  const slug = path.join(...params.slug);
   const post = await getPostBySlug(slug, [
-    'title',
-    'excerpt',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '', slug)
-  const linkMapping = await getLinksMapping()
-  const backlinks = Object.keys(linkMapping).filter(k => linkMapping[k].includes(post.slug) && k !== post.slug)
-  const backlinkNodes = Object.fromEntries(await Promise.all(backlinks.map(async (slug) => {
-    const post = await getPostBySlug(slug, ['title', 'excerpt']);
-    return [slug, post]
-  })));
+    "title",
+    "excerpt",
+    "date",
+    "slug",
+    "author",
+    "content",
+    "ogImage",
+    "coverImage",
+  ]);
+  const content = await markdownToHtml(post.content || "", slug);
+  const linkMapping = await getLinksMapping();
+  const backlinks = Object.keys(linkMapping).filter((k) =>
+    linkMapping[k].includes(post.slug) && k !== post.slug
+  );
+  const backlinkNodes = Object.fromEntries(
+    await Promise.all(backlinks.map(async (slug) => {
+      const post = await getPostBySlug(slug, ["title", "excerpt"]);
+      return [slug, post];
+    })),
+  );
 
   return {
     props: {
@@ -100,19 +106,19 @@ export async function getStaticProps({ params }: Params) {
       },
       backlinks: backlinkNodes,
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const posts = await getAllPosts(['slug'])
+  const posts = await getAllPosts(["slug"]);
   return {
     paths: posts.map((post) => {
       return {
         params: {
           slug: post.slug.split(path.sep),
         },
-      } 
+      };
     }),
     fallback: false,
-  }
+  };
 }

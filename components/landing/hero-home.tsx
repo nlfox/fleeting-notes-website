0ab48cdf1efd3dbb61144eb6052c8d305634d101
Matthew Wong <matthewwong525@gmail.com>
2023-09-11 22:19:04 -0400

    update learn more button

--- components/landing/hero-home.tsx
+++ components/landing/hero-home.tsx
@@ -54,7 +54,7 @@ const HeroHome = () => {
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://my.fleetingnotes.app">Try Web App</a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="">Learn more</a>
+                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://docs.fleetingnotes.app">Learn more</a>
                 </div>
               </div>
             </div>

e34d565cb0fa2ec9414689a5332fdf0185fd978f
Matthew Wong <matthewwong525@gmail.com>
2023-09-20 14:53:36 -0400

    dangerouslySEtHtml

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script>
-          {`
-            (function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');
-          `}
-        </script>
-        <script async src='https://r.wdfl.co/rw.js' data-rewardful='bbc0a9'></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,"rewardful");
+          `,
+        }}
+      ></script>
+        <script async src="https://r.wdfl.co/rw.js" data-rewardful="bbc0a9"></script>
       </Head>
       <body>
         <Main />
